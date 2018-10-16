abf4cd0fb1db8936dfcc89d56975e4bd0b762e00
sachidanand suraj <suraj.thepions@gmail.com>
2018-10-16 21:47:27 +0530

    Updated the file with feature scaling
    Updated the file with feature scaling

--- Data Preprocessing(Handling Missing Data)/categorical_data.R
+++ Data Preprocessing(Handling Missing Data)/categorical_data.R
@@ -18,3 +18,16 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased = factor(dataset$Purchased,
                            levels = c('No', 'Yes'),
                            labels = c(0, 1))
+
+#Splitting the data into the Training Set and Test Set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_Set = subset(dataset, split == TRUE)
+test_Set = subset(dataset, split == FALSE)
+
+#Feature Scaling
+training_Set[,2:3] = scale(training_Set[,2:3])
+test_Set[,2:3] = scale(test_Set[,2:3])
+
